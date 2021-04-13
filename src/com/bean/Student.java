b0f5264fb054e092f577b77ceef3555015d6569a
cy <2239145193@qq.com>
2021-04-13 15:24:57 +0800

    第一次eclipse测试

---
+++ src/com/bean/Student.java
@@ -0,0 +1,17 @@
+package com.bean;
+
+public class Student {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("9999999999999999999");
+		System.out.println("9999999999999999999");
+		System.out.println("9999999999999999999");
+		System.out.println("9999999999999999999");
+		System.out.println("9999999999999999999");
+		System.out.println("9999999999999999999");
+		System.out.println("9999999999999999999");
+		System.out.println("9999999999999999999");
+	}
+
+}
